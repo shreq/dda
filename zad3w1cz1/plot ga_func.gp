2b36fd28fec4bc698a4a064b48c9ed1a9f2c732d
shreq <shrek.kresh@o2.pl>
2018-06-12 20:55:51 +0200

    elo

---
+++ zad3w1cz1/plot ga_func.gp
@@ -0,0 +1,11 @@
+# setup
+set xlabel "X"
+set ylabel "Y"
+set grid
+set style function lines
+
+# action
+plot './zad3w1/bin/ga_func.dat' with linespoints ls 1
+
+#pause mouse any
+pause -1
--- zad3w1cz1/zad3w1/Program.cs
+++ zad3w1cz1/zad3w1/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿    using System;
+using System.IO;
 
 namespace zad3w1
 {
@@ -8,13 +9,14 @@ namespace zad3w1
 
         static void Main(string[] args)
         {
-            Console.WindowHeight *= 3;
             Console.WindowHeight /= 2;
+            Console.WindowHeight *= 3;
+            var fstream = new StreamWriter("../ga_func.dat");
 
             // set up and start population
             int    populationSize = 100;
-            double mutationRate   = 1;
-            bool   elitism        = true;
+            double mutationRate   = 0.5;
+            bool   elitism        = false;
             double wantedFitness  = 0.099; // function minimum is 10, so this value shouldn't be higher than 0.1
             Console.WriteLine("Population size:  " + populationSize.ToString() +
                             "\nMutation rate:    " + mutationRate.ToString() +
@@ -22,7 +24,7 @@ namespace zad3w1
                             "\nWanted fitness:   " + wantedFitness.ToString());
             population = new Population(populationSize, mutationRate, elitism);
 
-            // first generation
+        // first generation
             double bestFitness = 0;
             population.CountFitness();
             Console.WriteLine("\nGEN\t:  FITTEST'S FITNESS\t|  COORDINATES\t\t=  FUNCTION VALUE\n");
@@ -44,10 +46,13 @@ namespace zad3w1
                     Console.Write(" ");
                     PrintStatus();
                 }
+
+                fstream.WriteLine(population.generation.ToString() + '\t' + population.Function(population.GetFittest().genotype[0], population.GetFittest().genotype[1]).ToString());
             }
 
         // finish
             Console.WriteLine("\nDone");
+            fstream.Dispose();
             Console.Beep();
             Console.ReadKey();
         }
